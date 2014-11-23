7835ae2b79d4e4bdb91bc88fa5fa5f7fafb8bbad
samsam2310 <samsam2310@gmail.com>
2014-11-23 17:41:30 +0800

    fix static file setting

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import tornado.web
 import tornado.wsgi
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world!!!")
+    	self.render("twcl.html")
+        # self.write("Hello, world!!!")
 
+settings = {
+    'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+    'debug':False,
+}
 tornado_app = tornado.web.Application([
     (r"/", MainHandler),
-])
+],**settings)
 app = tornado.wsgi.WSGIAdapter(tornado_app)
