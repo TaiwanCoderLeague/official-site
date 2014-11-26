9ca5183e06a11370c1d8e7acb9fd66ad0271e68e
samsam2310 <samsam2310@gmail.com>
2014-11-26 18:42:13 +0800

    add some code to print the log

--- my_db.py
+++ my_db.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from google.appengine.ext import db
 
+import logging
+
 class Key(db.Model):
     username = db.StringProperty(required=True)
     passward = db.StringProperty(required=True)
@@ -9,6 +11,10 @@ class Key(db.Model):
         q = cls.all()
         return q.get()
 dbuser = Key.get()
+logging.info("database user = "+str(dbuser.username)+'  '+str(dbuser.passward))
+# if not dbuser:
+# 	dbuser = Key(username="twclorange",passward="twclorange7122")
+# 	dbuser.put()
 
 client = MongoClient('mongodb://'+dbuser.username+':'+dbuser.passward+'@ds053090.mongolab.com:53090/twclorange')
 database = client['twclorange'] 
