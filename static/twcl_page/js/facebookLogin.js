d76d38dcb83b125f1af69300eca7a445976d63b3
吳仲昇 <samsam2310@gmail.com>
2014-12-29 22:47:40 +0800

    fix bug

--- static/twcl_page/js/facebookLogin.js
+++ static/twcl_page/js/facebookLogin.js
@@ -1,14 +1,14 @@
 //Login Fb
 var fb_login_btn_lock = false;
 
-$('#fb_login_btn').click(function(){
+$('#fb-login-btn').click(function(){
     if(fb_login_btn_lock)return;
     fb_login_btn_lock = true;
     console.log('fb login');
     FB.login(function(response){
         if(response.authResponse){
-            $('login_form').append("<input type=\"test\" hidden name=\"fblogin\" value=\"1\"></input>")
-            $('login_form').submit();
+            $('#login-form').append("<input type=\"test\" hidden name=\"fblogin\" value=\"1\"></input>")
+            $('#login-form').submit();
         }
         console.log('fb return');
         fb_login_btn_lock = false;
